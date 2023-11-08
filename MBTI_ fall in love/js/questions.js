2965ae60b8bcd3bf742e2bf7e5011f44397bf3b2
Seok Hyeon <69139576+seokhyeon0916@users.noreply.github.com>
2023-11-08 13:49:00 +0900

    Update questions.js

--- MBTI_ fall in love/js/questions.js
+++ MBTI_ fall in love/js/questions.js
@@ -35,12 +35,69 @@ function nextQuestion(choiceNumber) {
 function showResultPage() {
   location.href = '/results.html?mbti=' + mbti
 }
-
+let m=0;/* MBTI E*/
+let M=0;/**MBTI I */
+let b=0;/**MBTI S */
+let B=0;/**MBTI N */
+let c=0;/**MBTI T */
+let C=0;/**MBTI F */
+let d=0;/**MBTI P */
+let D=0;/**MBTI J */
 // '답변1' 혹은 '답변2'를 클릭했을 때 동작하는 코드에요!
 choice1El.addEventListener('click', function () {
+  if(currentNumber===0){
+    m++;
+  }else if(currentNumber===1){
+    m++;
+  }else if(currentNumber===2){
+    m++;
+  } else if(currentNumber===3){
+    b++;
+  }else if(currentNumber===4){
+    b++;
+  }else if(currentNumber===5){
+    b++;
+  }else if(currentNumber===6){
+    c++;
+  }else if(currentNumber===7){
+    c++;
+  }else if(currentNumber===8){
+    c++;
+  }else if(currentNumber===9){
+    d++;
+  }else if(currentNumber===10){
+    d++;
+  }else if(currentNumber===11){
+    d++;
+  }
   nextQuestion(0)
 })
 choice2El.addEventListener('click', function () {
+  if(currentNumber===0){
+    M++;
+  }else if(currentNumber===1){
+    M++;
+  }else if(currentNumber===2){
+    M++;
+  } else if(currentNumber===3){
+    B++;
+  }else if(currentNumber===4){
+    B++;
+  }else if(currentNumber===5){
+    B++;
+  }else if(currentNumber===6){
+    C++;
+  }else if(currentNumber===7){
+    C++;
+  }else if(currentNumber===8){
+    C++;
+  }else if(currentNumber===9){
+    D++;
+  }else if(currentNumber===10){
+    D++;
+  }else if(currentNumber===11){
+    D++;
+  }
   nextQuestion(1)
 })
 
