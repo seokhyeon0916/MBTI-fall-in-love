1fabd8b4de79496b466e5791c193e0681db14278
Seok Hyeon <69139576+seokhyeon0916@users.noreply.github.com>
2023-11-09 15:25:35 +0900

    Update questions.js

--- MBTI_ fall in love/js/questions.js
+++ MBTI_ fall in love/js/questions.js
@@ -10,26 +10,14 @@ const choice2El = document.querySelector('.choice2')
 let currentNumber = 0 // 현재 질문 번호
 let mbti = '' // MBTI 결과
 
-// 화면에 질문을 랜더링하는 함수에요!
+// 화면에 질문을 랜더링하는 함수!
 function renderQuestion() {
   const question = questions[currentNumber]
   questionEl.innerHTML = question.question
   numberEl.innerHTML = question.number
   choice1El.innerHTML = question.choices[0].text
   choice2El.innerHTML = question.choices[1].text
-  progressValueEl.style.width = (currentNumber + 1) * 10 + '%'
-}
-// 다음 질문으로 넘어가는 함수에요!
-function nextQuestion(choiceNumber) {
-  // 더 이상 질문이 없으면, 결과 페이지를 보여줘요!
-  if (currentNumber === questions.length - 1) {
-    showResultPage()
-    return
-  }
-  const question = questions[currentNumber]
-  mbti = mbti + question.choices[choiceNumber].value
-  currentNumber = currentNumber + 1
-  renderQuestion()
+  progressValueEl.style.width = (currentNumber + 1) * 8.3 + '%'
 }
 // 결과 페이지로 이동!
 function showResultPage() {
@@ -43,7 +31,7 @@ let c=0;/**MBTI T */
 let C=0;/**MBTI F */
 let d=0;/**MBTI P */
 let D=0;/**MBTI J */
-// '답변1' 혹은 '답변2'를 클릭했을 때 동작하는 코드에요!
+// '답변1' 혹은 '답변2'를 클릭했을 때 동작하는 코드!
 choice1El.addEventListener('click', function () {
   if(currentNumber===0){
     m++;
@@ -70,6 +58,7 @@ choice1El.addEventListener('click', function () {
   }else if(currentNumber===11){
     d++;
   }
+  return m,b,c,d;
   nextQuestion(0)
 })
 choice2El.addEventListener('click', function () {
@@ -98,8 +87,32 @@ choice2El.addEventListener('click', function () {
   }else if(currentNumber===11){
     D++;
   }
+  return M,B,C,D;
   nextQuestion(1)
 })
+// 다음 질문으로 넘어가는 함수!
+function nextQuestion(choiceNumber) {
+  // 더 이상 질문이 없으면, 결과 페이지를 보여줌!
+  if (currentNumber === questions.length - 1) {
+    if (m > M) mbti += 'e';
+    else mbti += 'i';
 
+    if (b > B) mbti += 's';
+    else mbti += 'n';
+
+    if (c > C) mbti += 't';
+    else mbti += 'f';
+
+    if (d > D) mbti += 'p';
+    else mbti += 'j';
+
+    showResultPage()
+    return 
+  }
+  const question = questions[currentNumber]
+  mbti = mbti + question.choices[choiceNumber].value
+  currentNumber = currentNumber + 1
+  renderQuestion()
+}
 // 첫 번째 질문을 렌더링해요!
 renderQuestion()
