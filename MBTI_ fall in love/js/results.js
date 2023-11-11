1b3757049d5bd767b952b683389326fa805f1482
이승연 (Seung Yeon Lee) <90338530+MATILDASEUNGYEON@users.noreply.github.com>
2023-11-12 00:06:57 +0900

    Update results.js

--- MBTI_ fall in love/js/results.js
+++ MBTI_ fall in love/js/results.js
@@ -1,9 +1,9 @@
 import { results, mbtis } from './data.js'
 
 // 주소 쿼리스트링에서 mbti 값을 가져오기!
-const mbti = new URLSearchParams(location.search).get('mbti')
-const result = results[mbtis[mbti]]
-
+const mbtiResult = new URLSearchParams(location.search).get('mbtiResult1')
+const result = results[mbtis[mbtiResult]]
+// var mbtiresult = updateMBTI();
 // 결과를 출력할 각 요소를 찾아요!
 const titleEl = document.querySelector('.page-title')
 const characterEl = document.querySelector('.character')
@@ -12,10 +12,14 @@ const jobEls = document.querySelectorAll('.job')
 const lectureEl = document.querySelector('.lecture')
 const lectureImgEl = document.querySelector('.lecture img')
 
+//test
+console.log(mbtiResult);
+console.log(result);
+
 // 각 요소에 내용을 채워넣어요!
 titleEl.innerHTML = result.title
 characterEl.src = result.character
-boxEls.forEach(function (boxEl, index) {
+boxEls.forEach(function (boxEl,index) {
   boxEl.innerHTML = result.results[index]
 })
 jobEls.forEach(function (jobEl, index) {
